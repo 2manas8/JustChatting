2752781279b6833d9c8aa8c07140ea6328aa2869
Manas Pratim Das <dasmp123@gmail.com>
2024-07-19 23:34:28 +0530

    Updated the jwt token

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -16,6 +16,7 @@ exports.login = async (req, res) => {
         }
         res.status(200).json({
             token: jwt.sign({
+                id: user._id,
                 name: user.name,
                 email: user.email,
                 phone: user.phone
