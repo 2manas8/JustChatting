52244d2b2a377783c145b1b9893fa89cca44ec74
Manas Pratim Das <dasmp123@gmail.com>
2024-07-22 22:35:39 +0530

    Updated user_details.js

--- backend/controllers/user_details.js
+++ backend/controllers/user_details.js
@@ -1,17 +1,32 @@
 const User = require('../models/user')
 
+const otherUsersFilter = (myUsername, users) => {
+    const otherUsers = []
+    for(const user of users) {
+        if(user.username != myUsername) {
+            otherUsers.push(user)
+        }
+    }
+    return otherUsers
+}
+
 exports.userDetails = async (req, res) => {
     try {
+        const myUsername = req.query.my_username
         const username = req.query.username
-        const user = await User.findOne({username: username})
-        if(!user) {
+        const users = await User.find({username: new RegExp(username)})
+        const otherUsers = otherUsersFilter(myUsername, users)
+        if(users.length == 0) {
             return res.status(400).json({message: 'User does not exist'})
         }
-        res.status(200).json({
-            message: 'User found',
+        const userDetails = otherUsers.map((user) => ({
             userId: user._id,
             name: user.name,
             username: user.username
+        }))
+        res.status(200).json({
+            message: 'User found',
+            users: userDetails
         })
     } catch(error) {
         console.log(error)
